d67dd75864e2165ebfc3472f686be96c7fd87e47
Ben Firshman <ben@firshman.co.uk>
2014-09-23 18:16:03 -0700

    Wait for TCP in a loop

--- hosts/ssh/ssh.go
+++ hosts/ssh/ssh.go
@@ -49,13 +49,16 @@ func GenerateSSHKey(path string) error {
 }
 
 func WaitForTCP(addr string) error {
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	if _, err = conn.Read(make([]byte, 1)); err != nil {
-		return err
+	for {
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			continue
+		}
+		defer conn.Close()
+		if _, err = conn.Read(make([]byte, 1)); err != nil {
+			continue
+		}
+		break
 	}
 	return nil
 }
